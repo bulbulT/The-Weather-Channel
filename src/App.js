9581232f5d432f944a7f41e19a45c80c28b0d9ca
Bulbul Tripathy <Bulbul.Tripathy@sas.com>
2022-03-03 11:07:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
   return (
     <div className="App">
       <header className="d-flex justify-content-center align-items-center">
-        <h2>Look up the weather</h2>
+        <h2>The Weather Channel</h2>
       </header>
       <div className="container">
         <div className="mt-3 d-flex flex-column justify-content-center align-items-center">
